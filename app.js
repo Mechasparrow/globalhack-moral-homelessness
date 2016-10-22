462fe7d6419c85a237dc5c7d66301ba1674b2efb
michael <mikipux7@gmail.com>
2016-10-22 09:48:33 -0500

    added some stuff for the thing

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var express = require('express')
 var app = express()
+var gsjson = require('google-spreadsheet-to-json');
+
 
 app.use(express.static('public'));
-app.use(express.static('data'))
-app.get('/', function (req, res) {
-    res.send('Hello world');
+
+app.get('/clientdata', function (req, res) {
+    gsjson({
+      spreadsheetId: '1D5r0AQhFoyAWFh77Q_VPHpCN51_ETIIn_q3OSbbELIU',
+      token: "ya29.Ci-EAySu-jp3M3KOF3ZUW63CVymjoUOI_8bqUJF7Oj-jTV6Rhg4dzqYcgNqp_XXLsQ",
+      worksheet: "Funder"
+    }).then(function(result) {
+      res.json(result);
+    });
 });
 
 app.listen(3000)
