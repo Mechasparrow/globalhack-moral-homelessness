3af661d073105f01d7ed1b6f4c598fc2ab889bca
michael <mikipux7@gmail.com>
2016-10-22 14:57:46 -0500

    simple view

--- public/script.js
+++ public/script.js
@@ -1,8 +1,18 @@
 $(document).ready (function () {
-
-
+  getMoraldata();
 });
 
 function getMoraldata(){
-  
+  $.getJSON( "/clientdata", function( data ) {
+
+    for (var i = 0; i < data.length; i ++){
+
+      var tablerow = $("<tr></tr>");
+
+      tablerow.append('<td>' + data[i].First_Name +'</td>');
+      tablerow.append('<td>' + data[i].Last_Name +'</td>');
+
+      $("#homeless_data_table").append(tablerow);
+    }
+  });
 }
