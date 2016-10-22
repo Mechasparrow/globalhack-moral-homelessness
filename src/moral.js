9256e12aa644eb2a007d93d8dfbb3656da4e37f4
michael <mikipux7@gmail.com>
2016-10-22 12:30:00 -0500

    Added age priority

--- src/moral.js
+++ src/moral.js
@@ -3,9 +3,19 @@ var router = express.Router();
 var gsjson = require('google-spreadsheet-to-json');
 var token = require("./token.js");
 var moment = require('moment');
-var authtoken = "";
+var Converter = require('csvtojson').Converter;
+var converter = new Converter({});
+var fs = require("fs");
+
+var clientData = "";
 
 
+
+converter.on("end_parsed", function (jsonArray){
+  clientData = jsonArray;
+})
+
+fs.createReadStream("./data/client.csv").pipe(converter);
 
 token.getToken (function (token) {
   authtoken = token;
@@ -16,14 +26,7 @@ router.get('/clientdata', function (req, res) {
 });
 
 function clientdata(res, authtoken) {
-    gsjson({
-      spreadsheetId: '1D5r0AQhFoyAWFh77Q_VPHpCN51_ETIIn_q3OSbbELIU',
-      token: authtoken,
-      worksheet: "Client"
-    }).then(function(result) {
-      res.json(moralize(result));
-    });
-
+  res.json(moralize(clientData));
 }
 
 function moralize(data){
@@ -32,10 +35,13 @@ function moralize(data){
   var newdata = data;
 
   for (var i = 0; i < newdata.length; i ++){
-    newdata[i].priority = Math.random();
+    newdata[i].priority = 0;
 
-    var dobstring = newdata[i].dob;
-    console.log(dobstring);
+    var dobstring = newdata[i].DOB;
+    var age = moment().diff(moment(dobstring, 'MM/DD/YYYY'), 'years');
+    if (age >= 50 || age <= 10){
+      newdata[i].priority += 2;
+    }
   }
 
   return newdata
