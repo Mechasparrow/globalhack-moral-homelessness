953e4550b537f722533c198511a4e9feb0e1b4a4
marioluigiman <mikipux7@gmail.com>
2016-10-27 19:11:04 -0500

    Added port changing

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var app = express()
 
+var port = process.env.PORT || 8080;
+
 app.use(express.static('public'));
 
 app.use('/', require('./src/moral.js'));
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000')
+app.listen(port, function () {
+  console.log('Listening on port ' + port)
 });
